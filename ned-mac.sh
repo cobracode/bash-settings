993108e902d598041b28d29b0123008a00289f43
Ned M <cobracode@users.noreply.github.com>
2023-10-27 19:50:28 -0700

    Renaming to ned-mac.sh since differences between linux and mac commands

---
+++ ned-mac.sh
@@ -0,0 +1,16 @@
+echo 'BEGIN ned.sh'
+
+# Set variables -------------------------
+HISTSIZE=9999
+HISTFILESIZE=9999
+
+
+# Aliases -------------------------------
+
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -a --classify --human-readable -l --reverse -t'
+
+echo 'TODO: Add datetime at start, end'
+
+echo 'END ned.sh'
